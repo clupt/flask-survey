c647f8eb2e2d9c90cb8e25b258427665734f1568
sarah <sarahgraup@g.ucla.edu>
2023-03-09 16:41:00 -0800

    first portion of app

--- app.py
+++ app.py
@@ -13,17 +13,40 @@ responses = []
 @app.get("/")
 def show_survey():
     """ Returns a rendered template of the survey"""
+
     return render_template("survey_start.html", survey = survey)
 
 @app.post("/begin")
 def direct_user_to_question():
     """ Brings the user to questions"""
+    #empty out list of responses
+    responses.clear()
     return redirect("/questions/0")
 
 @app.get("/questions/<int:question_num>")
 def show_current_question(question_num):
     """ Takes in question number and shows html for form for that question"""
+
     question = survey.questions[question_num]
     return render_template("question.html",
                            question_num = question_num,
                            question = question)
+
+@app.post("/answer")
+def store_answer_redirect():
+    """access and save response and redirect to next question"""
+
+    choice = request.form['answer']
+    responses.append(choice)
+    if(len(responses) == len(survey.questions)):
+        return redirect("/completion")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+@app.get("/completion")
+def completion():
+    """sends questions and user reponses to completion page"""
+
+    questions = survey.questions
+    return render_template(
+        "completion.html", questions=questions, responses=responses)
